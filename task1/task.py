c24e508b6418376952d709e096b1b7f911370f0d
Vindalamar <ankovol2010@gmail.com>
2023-09-28 08:33:25 +0300

    update

--- task1/task.py
+++ task1/task.py
@@ -1,18 +1,19 @@
 import csv
 
 
-def read_line(path, row_c):
+def read_line(path, row_c, col_c):
     with open(path, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         i = 0
         for row in spamreader:
             if i == row_c:
-                print(', '.join(row))
+                print(row[col_c])
+                break
             i += 1
 
 
 
 
 if __name__ == '__main__':
-    read_line('example.csv', 1)
+    read_line('example.csv', 1, 5)
 
